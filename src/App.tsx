4380b0bc7ba753b17599c2d8a78652e9de2a3b27
himalMajumder <himalmajumder@gmail.com>
2023-04-05 00:27:08 +0600

    Basic Props practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,43 @@
 import './App.css'; 
+import { Greet } from './components/Greet';
+import { Heading } from './components/Heading';
+import { Oscar } from './components/Oscar';
+import { Person } from './components/Person';
+import { PersonList } from './components/PersonList';
+import { Status } from './components/Status';
 
 function App() {
+  const personName = {
+    firstName : 'Bruce',
+    lastName : 'Wayne'
+  }
+
+  const nameList = [
+    {
+      first : "Bruce",
+      last : "Wayne"
+    },
+    {
+      first : "Clark",
+      last : "Kent"
+    }, 
+    {
+      first : "Princess",
+      last : "Diana"
+    }
+  ];
+
   return (
     <div className="App">
-      <h2>Lets Start Typescript </h2>
+      {/* <Greet name="Himal" messageCount={10} isLoggedIn={true} /> */}
+      <Greet name="Himal"  isLoggedIn={true} />
+      <Person name={personName} />
+      <PersonList names={nameList}/>
+      <Status status='loading' />
+      <Heading> Placeholder text </Heading>
+      <Oscar>
+        <Heading>Oscar goes to Leonardo Dicpario!</Heading>
+      </Oscar>
     </div>
   );
 }
